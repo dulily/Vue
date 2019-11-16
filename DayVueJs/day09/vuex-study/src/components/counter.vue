9559141b34e69f2547c234aea2399618263854f6
杜丽 <1737770335@qq.com>
2019-11-16 21:58:35 +0800

    vuex study

---
+++ DayVueJs/day09/vuex-study/src/components/counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <input type="button" value="减少" @click="remove">
+      <input type="button" value="增加" @click="add">
+      <br>
+      <input type="text" v-model="$store.state.count">
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        add(){
+            this.$store.commit('increment');
+        },
+        remove(){
+            // this.$store.commit('subtract',3);
+            this.$store.commit('subtract',{ a:1,b:3 });
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
